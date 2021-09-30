fedfbcae8baf40df922f1e4b8d1885c579ae636d
pablomedinasanz <90444657+pablomedinasanz@users.noreply.github.com>
2021-09-30 13:26:55 +0200

    Add files via upload

--- procesoPadre.java
+++ procesoPadre.java
@@ -4,27 +4,44 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class procesoPadre {
+    // PROCESO PADRE QUE PEDIRÁ PALABRAS POR TECLADO Y LLAMARÁ
+    // AL PROCESO HIJO TANTAS VECES COMO CARÁCTERES TENGA LA PALABRA INTRODUCDA
 
     private static String muestra;
 
     public static void main(String[] args) {
 
+        // instrucciones
         System.out.println(
                 "Este programa genera tantos números aleatorios como carácteres tenga la palabra que introduzca.");
-        System.out.println("El programa finalizará cuando introduzca la palabra 'fin'. ");
-        System.out.println("Introduzca una palabra: ");
+        System.out.println("El programa finalizará cuando introduzca la palabra 'fin'.");
+
+        // pide palabra por teclado
         Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca una palabra: ");
         String input = sc.nextLine();
 
-        for (int i = 0; i < input.length(); i++) {
+        // mientras la palabra no valga "fin" ejecuta el proceso hijo y pide una nueva
+        // palabra
+        while (!"fin".equalsIgnoreCase(input)) {
 
-            creaciónDeProcesoHijo();
+            System.out.println("Se van a imprimir " + input.length() + " números.\n");
+
+            for (int i = 0; i < input.length(); i++) {
+
+                creaciónDeProcesoHijo();
+            }
+            System.out.println("Introduzca una palabra: ");
+            input = sc.nextLine();
         }
+
+        System.out.println("FIN DEL PROGRAMA");
 
         sc.close();
 
     }
 
+    // método que crea el proceso hijo que genera un número aleatorio
     public static void creaciónDeProcesoHijo() {
 
         ProcessBuilder procesoHijo = new ProcessBuilder();
